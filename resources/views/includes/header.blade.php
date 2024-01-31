361ca44028708172bf8989b41a6bdec869678860
Jolas Casallas <casallasjolas28@gmail.com>
2024-01-31 23:15:01 +0800

    feat(layouts): add header and body layouts

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,5 @@
+<header class="flex justify-between items-center py-2 px-2">
+    <div class="flex items-center">
+        <h1 class="text-2xl font-extralight">Post Management System</h1> 
+    </div>
+</header>
---
+++ resources/views/pages/posts/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title')
+    Post Management | Create Post
+@endsection
+
+@section('content')
+    <main class="rounded-tl-lg bg-gray-50">
+        <div class="p-4 space-y-3">
+            <div class="flex flex-auto justify-between">
+                <div>
+                    <h1 class="text-xl font-medium">Hi Jolas</h1>
+                    <h1 class="text-sm font-light">Manage any type of project. Assign owners, set timelines and keep track of where your project stands.</h1>
+                </div>
+            </div>
+            <div class="flex justify-between">
+                fdsfsd  
+            </div>
+            <div class="flex justify-between">
+                fdsfsd  
+            </div>
+        </div>
+    </main>
+@endsection
